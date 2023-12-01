0bba3cfb94287d8ea21bc5df8a5d9dd179ce65d2
wusthq <143711637+wusthq@users.noreply.github.com>
2023-12-01 02:12:43 +0100

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -6,13 +6,19 @@ export async function handler (event, context) {
 	try {
 		const data = JSON.parse(event.body);
 		const { pageURL } = data;
+		const apiKey = process.env.DEEPL_API_KEY;
 
-		const res = await fetch(pageURL);
-		const htmlContent = await res.text();
+		const res = await fetch(pageURL, {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `DeepL-Auth-Key ${apiKey}`,
+			},
+		});
+		const jsonContent = await res.json();
 
 		return {
 			statusCode: 200,
-			body: htmlContent,
+			body: jsonContent,
 		};
 	} catch (e) {
 		let responseBody = "Something bad happened!";
